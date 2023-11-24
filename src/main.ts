de7bc8da1e16bc8554abee7a194dcea7294d0a5e
carlos <uppaplicativo@gmail.com>
2023-11-24 16:09:05 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // ou a origem específica que deseja permitir
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
   await app.listen(3000);
   
 }
